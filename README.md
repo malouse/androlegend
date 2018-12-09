2e5bba5c4cd572e59148a79bd77a44c72ef606b7
malouse <45729242+malouse@users.noreply.github.com>
2018-12-09 12:11:35 +0100

    new website

--- README.md
+++
@@ -1,2 +0,0 @@
-# androlegend
-androlegend
--- wp-config.php
+++ wp-config.php
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'W9m8~zV8J02{,L/NBs#7U4wJgF&=[[:JM<M{XM]|bb25ZKYzssyN$9Wm5;K@ow`@');
-define('SECURE_AUTH_KEY',  'KKxvV?{m}CYq[K>9e0?t1sgE4_+&LlpIq8k{4Bj7RIS9)p95}4a_2I/yhv!}g84l');
-define('LOGGED_IN_KEY',    ')sSPnX,dm~=^a|Qu]7GEY7+tG#$EKBH=E-9N|(TRm C)`Ouu2W45^`h8.@Mj^dIF');
-define('NONCE_KEY',        'K$QCx!.u3ywA_1BZMWDoy^$,PBv?Kz[6@uRlL%pcYZn?(lJ9-L=(BlkD<M#0$jL1');
-define('AUTH_SALT',        '%)z,^Ja u<FUCosZi]1$1w^bHOB!KD#,?]@@@77YCio?@GYQ!{eE>d]~Jof+n}m4');
-define('SECURE_AUTH_SALT', 'Lzrj[ CeWtGq2MsFk.x(5/}?(jLZP*&<Y5y(@!7F#(3(6c()S#]!z_k]^kq*)9c@');
-define('LOGGED_IN_SALT',   'ns>TWTq|4^YgMB,S@O{r4o|>c}0(&};<kj;3w,wwG 1s:[QVuPQjYwOyc_6 %hkg');
-define('NONCE_SALT',       'MGAyDi4w!%*E1PR ;>{s1G*)y0AN(>~12Y!I+n#6J8Lr-:nHn4>pTM39VMP+#)J0');
+define('AUTH_KEY',         '/evNx&x3ey#Yk?DvjF>s zp,=xu&|@a[v^6u.%i%2}X>xhIrI,]X.8iz,5De8THF');
+define('SECURE_AUTH_KEY',  ',=<w6MPnBR^$H^5}!z}_E#VxL8%,;kx?tW%Aj/ybIS`4%UA_M7pdFJ<g)>C`@{+7');
+define('LOGGED_IN_KEY',    ' jL=;RP$x6#X,4|Lgfc:i>6 F2nc.|WtqB(Sx4Y,KM=4l2{|MDzlooLV)Qse1%w|');
+define('NONCE_KEY',        '33wZJ=>wB#P8H{ Ei*xQL+]p1w#{3#f?pn:CQ`{@7H$:2wo,H_|0&_$<v|MWVh{~');
+define('AUTH_SALT',        'vT.z6RgbGX {B$A6%1sA47[WU]U_l(hx(sttDg37;4l~}T$x2;Isv_lyN3{LXEl,');
+define('SECURE_AUTH_SALT', 'tTF{lgN66si:myvZ&,`nJ(rW2`2U-#_9@{Lrk>h=::uht>[Et@W2[,ExZBxh5ys7');
+define('LOGGED_IN_SALT',   'S&en<>j@u $laS:rX+{7(N_<|Oe3hDO4qF-nn%&!,))51h3c|_0?Qr^&.LmC?EH&');
+define('NONCE_SALT',       ':Z{DgA?e+:[nOpX)ZD{YQX=Z9`*iecp:[*f3aC:Z3KuJQ^MMVFxSrxLpPH/dz=ai');
 
 /**#@-*/
 
